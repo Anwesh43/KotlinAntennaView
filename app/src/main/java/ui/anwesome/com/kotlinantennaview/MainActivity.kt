f18c51fe13a9b62cc27ba801c9068ba1cfe334f5
Anwesh43 <anweshthecool0@gmail.com>
2018-01-31 02:15:51 +0530

    adding antenna view in Activity

--- app/src/main/java/ui/anwesome/com/kotlinantennaview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinantennaview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinantennaview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.antennaview.AntennaView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        AntennaView.create(this)
     }
 }
