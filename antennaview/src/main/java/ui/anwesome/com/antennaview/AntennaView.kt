f5c69f4fe07b0a790daa3a5b610a1127421526f8
Anwesh43 <anweshthecool0@gmail.com>
2018-01-31 01:08:40 +0530

    adding AntennaState

--- antennaview/src/main/java/ui/anwesome/com/antennaview/AntennaView.kt
+++ antennaview/src/main/java/ui/anwesome/com/antennaview/AntennaView.kt
@@ -40,7 +40,24 @@ class AntennaView(ctx:Context):View(ctx) {
         }
         fun stop() {
             if(animated) {
-                animated = false 
+                animated = false
+            }
+        }
+    }
+    data class AntennaState(var scale:Float = 0f,var dir:Float = 0f,var prevScale:Float = 0f) {
+        fun update(stopcb:(Float)->Unit) {
+            scale += 0.1f*dir
+            if(Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                stopcb(scale)
+            }
+        }
+        fun startUpdating(startcb:()->Unit) {
+            if(dir == 0f) {
+                dir = 1 - 2 * scale
+                startcb()
             }
         }
     }
